02198b65473ad5ee9f78759270cf88d9feca12c4
Ajileye Boluwatife <boluwatifeajileye@gmail.com>
2022-11-03 15:05:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ header("Content-Type: application/json;");
     $json = file_get_contents('php://input');
     $data = json_decode($json);
 
- echo json_encode($data.result);
+ echo json_encode($data['x']);
 
 ?>
