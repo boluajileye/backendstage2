4ff9ed5162c8c32f20b2e48f1903cae44d03be30
Ajileye Boluwatife <boluwatifeajileye@gmail.com>
2022-11-03 17:09:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,41 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json;");
 
     
-    $json = file_get_contents('php://input');
-    $data = json_decode($json, true);
+    $jbolu = file_get_contents('php://input');
+    $data = json_decode($bolu, true);
 
     $operation = $data['operation_type'];
     $x = $data['x'];
     $y = $data['y'];
 
-    $bolu = [
-  "slackUsername"=>"ajileye",
-  "result"=> $operation,
-       "x"=> $x,
-        "y"=> $y
+
+    switch ($result) {
+    case '$operation == "addition"':
+       $result = $x + $y;
+        break;
+    case '$operation == "subtraction"':
+       $result = $x - $y;
+        break;
+    case '$operation == "multiplication"':
+       $result = $x * $y;
+        break;
+    case '$operation == "division"':
+       $result = $x / $y;
+        break;
+    default:
+        $result = "Invalid Operation Used"
+        break;
+}
+
+
+    $ajileyebolu = [
+  "slackUsername"=>"ajileyebolu",
+  "operation_type"=> $operation,
+   "result"=> $result
        ];
 
 
- echo json_encode($bolu);
+ echo json_encode($ajileyebolu);
 ?>
 
 
