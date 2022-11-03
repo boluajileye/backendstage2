24223fe7bd98b1c45ec6d0ac1e1d200db09e7bec
Ajileye Boluwatife <boluwatifeajileye@gmail.com>
2022-11-03 14:55:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json;");
 
+        $hng = [
+            "slackUsername"=> "ajileyebolu",
+            "backend"=>TRUE,
+            "age"=> 23,
+            "bio"=> "I am Ajileye Boluwatife, A recent graduate transitioning into tech. I'm so glad to be part of HNG9 interns"
+        
+        ];
+    
+    echo json_encode($hng);
+
     
     $json = file_get_contents('php://input');
-// Converts it into a PHP object 
-$data = json_decode($json, true);
+    $data = json_decode($json);
 
  echo json_encode($data);
 
